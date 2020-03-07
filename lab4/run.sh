e646d87f73df3692c0458b514cac0a5b7e7b2aca
root <demirk@hotmail.com>
2020-03-07 12:08:32 +0300

    lxc güncelleme

--- lab4/run.sh
+++ lab4/run.sh
@@ -1,11 +1,11 @@
 #LXC uygulamasi
 lxc-ls
 ls /usr/share/lxc/templates/
-lxc-create -n ubuntu_lxc -t ubuntu
+lxc-create -n mylxc -t busybox
 lxc-ls
 lxc-ls --active
-lxc-info --name ubuntu_lxc
-lxc-start -n ubuntu_lxc -d
-lxc-console -n ubuntu_lxc -t 0
-lxc-stop -n ubuntu_lxc
+lxc-info --name mylxc
+lxc-start -n mylxc -d
+lxc-console -n mylxc -t 0
+lxc-stop -n mylxc
 
